63ddeb6a53c62549e4c91a0b1ae7e18c6826e5e9
gs <gs@13047901299.139>
2018-09-06 19:00:43 +0800

    project init

---
+++ src/main/java/com/dk/gfm/GfmApplication.java
@@ -0,0 +1,12 @@
+package com.dk.gfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfmApplication.class, args);
+	}
+}
