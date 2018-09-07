062928cb5e4d4119254eea94b6d55af81a3652ad
gs <gs@13047901299.139>
2018-09-07 18:58:20 +0800

    使用freemark模板，实现html和css分离

---
+++ src/main/java/com/dk/gfm/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.dk.gfm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @ClassName HomeController
+ * @Description TODO
+ * @Author xiadekang
+ * @Date 2018/9/7 9:59
+ * @Version 1.0
+ **/
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "index")
+    public String index(){
+
+        System.out.println("我进来了");
+        return "index";
+    }
+}
